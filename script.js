86d521ccb93f15f162a95acfb02fedaefd05c0ff
JafarHasan <159635643+JafarHasan@users.noreply.github.com>
2024-08-13 21:58:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function convertToUppercase() {
+            var inputField = document.getElementById("fname");
+            inputField.value = inputField.value.toUpperCase();
+        }
+//let name=document.getElementById("fname").value.;
+
